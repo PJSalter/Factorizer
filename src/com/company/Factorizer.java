37d064506a2884d3d770d87b5a2792dca0445089
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-05-04 23:28:45 +0100

    Factorizer class that encapsulates with constructor that initializes the number and factors list and created private method

--- src/com/company/Factorizer.java
+++ src/com/company/Factorizer.java
@@ -1,4 +1,22 @@
 package com.company;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Creating a Factorizer class that encapsulates the factorization logic.
 public class Factorizer {
+    private int number;
+    private List<Integer> factors;
+
+    // Creating a constructor that initializes the number and factors list.
+    public Factorizer(int number) {
+        this.number = number;
+        this.factors = new ArrayList<>();
+        factorize();
+    }
+
+    // Created a private method that calculates the factors of the number.
+    private void factorize() {
+    }
 }
