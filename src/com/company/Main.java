f304cc001c752a7711b7686d3bf9c1027adb921f
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-05-23 23:29:46 +0100

    Added user input functionality to factorize a number and display the factorization results.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What number would you like to factor? ");
+        int number = scanner.nextInt();
+        Factorizer factorizer = new Factorizer(number);
+        factorizer.printfactorizationResults();
     }
 }
